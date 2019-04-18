4f307acb2a935b13b2e3286f8845784664d78146
kenwrites <kenwrites@users.noreply.github.com>
2019-04-18 11:35:20 -0400

    adding catcherror-test code

--- src/app/catcherror-test/catcherror-test.component.ts
+++ src/app/catcherror-test/catcherror-test.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { of } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
 
 @Component({
   selector: 'app-catcherror-test',
@@ -10,6 +12,23 @@ export class CatcherrorTestComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    this.testCatchError();
   }
+
+  testCatchError() {
+    of(1, 2, 3, 4, 5).pipe(
+      map(n => {
+        if (n === 4) {
+          // tslint:disable-next-line: no-string-throw
+          throw 'four!';
+        }
+        return n;
+      }),
+      catchError(err => of('error!')),
+    )
+      .subscribe(x => console.log(x));
+  // 1, 2, 3, I, II, III, IV, V
+  }
+
 
 }
