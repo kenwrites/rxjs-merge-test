e96f9ffcbe07e6e7e9e45623aa6f8cfbd0a08ddc
kenwrites <kenwrites@users.noreply.github.com>
2019-04-18 14:41:56 -0400

    finalizing return-on-complete

--- src/app/return-on-complete/return-on-complete.component.ts
+++ src/app/return-on-complete/return-on-complete.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-return-on-complete',
@@ -10,6 +11,27 @@ export class ReturnOnCompleteComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    console.log(this.returnOnComplete());
+  }
+
+  returnOnComplete(): any {
+    const array = [];
+
+    const observable = of(1, 2, 3);
+
+    const observer = {
+      next: x => {
+        array.push(x);
+        console.log(`array: ${array}`);
+      },
+      error: error => console.error(error.message),
+      complete: () => {
+        return array;
+      }
+    };
+
+    observable.subscribe(observer);
+
   }
 
 }
